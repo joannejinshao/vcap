04c8082761292156c0412d2523c97cae2672a2bb
Jin Shao <shaojinvivian@gmail.com>
2011-08-23 23:08:44 -0700

    remoing unnecessary files

--- bin/health_manager.rb
+++
@@ -1,3 +0,0 @@
-home = File.join(File.dirname(__FILE__), '../health_manager')
-ENV['BUNDLE_GEMFILE'] = "#{home}/Gemfile"
-require File.join(home, 'lib/health_manager')
--- health_manager/bin/health_manager.rb
+++
@@ -1,3 +0,0 @@
-home = File.join(File.dirname(__FILE__), '/..')
-ENV['BUNDLE_GEMFILE'] = "#{home}/Gemfile"
-require File.join(home, 'lib/health_manager')
